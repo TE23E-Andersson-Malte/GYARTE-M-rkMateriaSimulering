2bdb4696fd3865e29ee1ed54a39496656618fa14
Malte Andersson <malte.andersson@elev.ga.ntig.se>
2026-01-09 15:31:13 +0100

    optimisering
    Co-authored-by: Te23E-Alvin-Erngren <AlvinErn@users.noreply.github.com>

--- darkmattersim(leapfrog).py
+++ darkmattersim(leapfrog).py
@@ -7,23 +7,33 @@ import matplotlib.pyplot as plt
 # Konstanter och simuleringens parametrar
 #------------------------------
 
-G = 6.67e-11              # Gravitationskonstant
-h = 0.01               # Tidssteg
-t_max = 1             # Total simuleringstid
+G = 6.67e-11          # Gravitationskonstant
+h = 0.01              # Tidssteg
+t_max = 5             # Total simuleringstid
 steg = int(t_max / h) # Antal steg
+eps = 0.1
 
 #------------------------------
 # Funktion: total gravitationskraft på partikel i
 #------------------------------
+
+#def total_gravitationskraft(i, Positioner, Massor):
+#    kraft = np.zeros(2)
+#    for j in range(len(Massor)):
+#        if i != j:
+#            diff = Positioner[j] - Positioner[i]
+#            dist = np.linalg.norm(diff)
+#            if dist > 1e-10:
+#                kraft += G * Massor[i] * Massor[j] * diff / dist**3 #Newtons gravitationslag
+#    return kraft
 
 def total_gravitationskraft(i, Positioner, Massor):
     kraft = np.zeros(2)
     for j in range(len(Massor)):
         if i != j:
             diff = Positioner[j] - Positioner[i]
-            dist = np.linalg.norm(diff)
-            if dist > 1e-10:
-                kraft += G * Massor[i] * Massor[j] * diff / dist**3 #Newtons gravitationslag
+            dist = np.sqrt(np.sum(diff**2) + eps**2)
+            kraft += G * Massor[i] * Massor[j] * diff / dist**3 #Newtons gravitationslag
     return kraft
 
 #------------------------------
@@ -31,7 +41,7 @@ def total_gravitationskraft(i, Positioner, Massor):
 #------------------------------
 
 N = 10
-Massor = np.random.uniform(1, 10, size=N)  * 10e7     # Massor
+Massor = np.random.uniform(1, 10, size=N)  * 1e8     # Massor
 Positioner = np.random.uniform(-1, 1, size=(N, 2))    # Startpositioner
 Hastigheter = np.zeros((N, 2))                        # Börjar stillastående
 
@@ -75,6 +85,13 @@ for tid in range(steg):
 # Plotta partiklarnas banor
 #------------------------------
 
+def formatera_massa(m):
+    s = f"{m:.2e}"           # t.ex. '3.35e+08'
+    base, exp = s.split("e") # base='3.35', exp='+08'
+    exp = int(exp)           # gör om till heltal → 8
+    return f"{base} × 10^{exp}"
+
+
 plt.figure(figsize=(12, 8))
 
 for i in range(N):
@@ -84,7 +101,7 @@ for i in range(N):
     plt.plot(
         Positions_historik[:, i, 0],
         Positions_historik[:, i, 1],
-        label=f"P{i+1}: m={Massor[i]:.2f}, start=({start_x},{start_y})"
+        label = f"P{i+1}: m={formatera_massa(Massor[i])}, start=({start_x},{start_y})"
     )
 
 plt.title("Partiklarnas banor i 2D (Leapfrog)")
@@ -92,4 +109,4 @@ plt.xlabel("x-position")
 plt.ylabel("y-position")
 plt.grid(True)
 plt.legend()
-plt.show()
+plt.show(block = True)
